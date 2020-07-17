4a70a2cb81ce83bfb09290831bab1988dc6b4d85
jasimkasba <saldanodi88@gmail.com>
2020-07-17 23:53:26 +0600

    git commit

---
+++ assignment.js
@@ -0,0 +1,16 @@
+// feetTomile
+function feetToMile(feet){
+    var mile = feet/5280;
+    return mile;
+}
+
+// woodcalculator
+
+function woodCalculator( chair, table, bed){
+    var chairCount = chair * 1;
+    var tableCount = table * 3;
+    var bedCount = bed * 5;
+    var totalWood = chairCount + tableCount + bedCount;
+    return totalWood;
+}
+// end.
